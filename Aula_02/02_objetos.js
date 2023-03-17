b20ee6cfd1e64aa9f522ee1ce52618f66d527def
Camila Belmont <camilabelmont.alu@gmail.com>
2023-03-17 11:27:24 -0300

    update

---
+++ Aula_02/02_objetos.js
@@ -0,0 +1,18 @@
+// let picachu = {
+//     nome: "Picachu",
+//     level: 1,
+//     tipo: "Elétrico"
+// };
+// console.log(picachu.nome);
+// picachu.level ++;
+// console.log(picachu.level);
+var Pokemon = /** @class */ (function () {
+    function Pokemon() {
+    }
+    return Pokemon;
+}());
+var pokemon = new Pokemon();
+pokemon.name = "Pikachu";
+pokemon.level = 15;
+pokemon.type = "Eletrico";
+console.log(pokemon);
---
+++ Aula_02/02_objetos.ts
@@ -0,0 +1,24 @@
+// let picachu = {
+//     nome: "Picachu",
+//     level: 1,
+//     tipo: "Elétrico"
+// };
+
+// console.log(picachu.nome);
+
+// picachu.level ++;
+// console.log(picachu.level);
+
+class Pokemon {
+    name: string;
+    level: number;
+    type: string;
+}
+
+let pokemon = new Pokemon();
+
+pokemon.name = "Pikachu";
+pokemon.level = 15;
+pokemon.type = "Eletrico";
+
+console.log(pokemon);
