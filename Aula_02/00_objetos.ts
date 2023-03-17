63a42a25b7316d19674d1ba4d58091ad5d6f4c18
Camila Belmont <camilabelmont.alu@gmail.com>
2023-03-17 11:32:34 -0300

    update

---
+++ Aula_02/00_objetos.ts
@@ -0,0 +1,10 @@
+let picachu = {
+    nome: "Picachu",
+    level: 1,
+    tipo: "Elétrico"
+};
+
+console.log(picachu.nome);
+
+picachu.level ++;
+console.log(picachu.level);
--- Aula_02/01_class.ts
+++ Aula_02/01_class.ts
@@ -1,14 +1,3 @@
-// let picachu = {
-//     nome: "Picachu",
-//     level: 1,
-//     tipo: "Elétrico"
-// };
-
-// console.log(picachu.nome);
-
-// picachu.level ++;
-// console.log(picachu.level);
-
 class Pokemon {
     name: string;
     level: number;
